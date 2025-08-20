acb5f6ee4ae9798408a01e7dbcb615f2ef2fb60f
Jorge Luis Hita, Ph.D. <jluishita@gmail.com>
2025-08-20 18:32:12 +0200

    init file

--- src/observational_learn/evaluate/__init__.py
+++ src/observational_learn/evaluate/__init__.py
@@ -1,1 +1,9 @@
+from ._pairbalance import StandarizedDifferences
+from ._boxplots import compare_boxplots
+from ._contingency_tables import contingency_table
 
+__all__ = [
+    "StandarizedDifferences",
+    "compare_boxplots",
+    "contingency_table",
+]
