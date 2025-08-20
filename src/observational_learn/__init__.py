fb06a63e9ead912b92f746cf168ff8e9a5ce12a2
Jorge Luis Hita, Ph.D. <jluishita@gmail.com>
2025-08-20 18:31:21 +0200

    init file

--- src/observational_learn/__init__.py
+++ src/observational_learn/__init__.py
@@ -1,1 +1,11 @@
+from observational_learn.datatable._datatable import DataTable
 
+__all__ =  [
+    "matching",
+    "sensitivity",
+    "evaluate",
+    "DataTable"
+]
+
+def __dir__():
+    return __all__
