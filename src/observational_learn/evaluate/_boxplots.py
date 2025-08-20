fdf4e9a1a7410a8a338aabb3540de8ae013f98c1
Jorge Luis Hita, Ph.D. <jluishita@gmail.com>
2025-08-20 18:34:55 +0200

    add boxplots

--- src/observational_learn/evaluate/_boxplots.py
+++ src/observational_learn/evaluate/_boxplots.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+
+def compare_boxplots(datatable, variable, **kwargs):
+    """
+    """
+    sns.boxplot(
+        data=datatable.dataframe, 
+        x="treat", 
+        y=variable,
+        **kwargs)
